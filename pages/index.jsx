c204ce64f5c9f494ba792270ae9824a7a22766db
Luis Abellan <luis.abellan.dev@gmail.com>
2022-11-15 17:29:35 +0100

    save

--- pages/index.jsx
+++ pages/index.jsx
@@ -229,7 +229,7 @@ export default function Home() {
       {currentAccount && (memos.map((memo, idx) => {
         return (
           <div key={idx} style={{border:"2px solid", "borderRadius":"5px", padding: "5px", margin: "5px"}}>
-            <p style={{"fontWeight":"bold"}}>&quot{memo.message}&quot</p>
+            <p style={{"fontWeight":"bold"}}>{memo.message}</p>
             <p>From: {memo.name} at {memo.timestamp.toString()}</p>
           </div>
         )
